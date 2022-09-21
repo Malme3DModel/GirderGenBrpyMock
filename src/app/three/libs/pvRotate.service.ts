73529971aea378d3445fd01675246f42899d1f04
yuluji <yuji.hirano@malme.co.jp>
2022-09-21 17:13:16 +0900

    投影法を平行投影法に変更

--- src/app/three/libs/pvRotate.service.ts
+++ src/app/three/libs/pvRotate.service.ts
@@ -12,17 +12,12 @@ export class pvRotateService {
 
     const origine = new THREE.Vector3(point[0], point[1], point[2]);
 
-    const x = Math.floor(x_rotate);
-    const y = Math.floor(y_rotate);
-    const z = Math.floor(z_rotate);
+    const x = Math.floor(x_rotate * Math.PI / 180.0);
+    const y = Math.floor(y_rotate * Math.PI / 180.0);
+    const z = Math.floor(z_rotate * Math.PI / 180.0 + Math.PI / 4.0);
 
     let Mesh = obj.clone();
-    if (!obj.position.equals(origine)) {
-      // 回転中心が異なっていたら、回転中心を持つGroupを作って登録する
-      Mesh = new THREE.Group();
-      Mesh.position.set(origine.x, origine.y, origine.z);
-      Mesh.add(obj)
-    }
+
 
     const Model_x = Mesh.rotateX(x);
     const Model_xy = Model_x.rotateY(y);
