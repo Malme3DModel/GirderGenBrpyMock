842544862fbc633c5951291d776cb5579d9561b7
sasaco <sasaicco@gmail.com>
2022-09-22 23:31:56 +0900

    チャット部分を

--- src/app/component/chat/chat.component.ts
+++ src/app/component/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { SocketioService } from './socketio-service.service';
 
@@ -8,18 +8,10 @@ import { SocketioService } from './socketio-service.service';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit, OnDestroy {
-
+  
   constructor(
-    private socketService: SocketioService,
+    public socketService: SocketioService,
     public dialogRef: MatDialogRef<ChatComponent>) { }
-
-  public comments: { user: boolean, content: string }[] = [
-    { user: true, content: '１つ目のコメントです。' },
-    { user: false, content: '２つ目のコメントです。' },
-    { user: true, content: '３つ目のコメントです。' }
-  ];
-
-  public Message: string = '';
 
   ngOnInit(): void {
     this.socketService.setupSocketConnection();
@@ -29,8 +21,8 @@ export class ChatComponent implements OnInit, OnDestroy {
     this.socketService.disconnect();
   }
 
-  public sendMessage(): void {
-    this.socketService.sendMessage(this.Message + 'aaa');
+  public sendMessage(Message : string): void {
+    this.socketService.sendMessage(Message);
   }
 
 }
--- src/app/component/chat/socketio-service.service.ts
+++ src/app/component/chat/socketio-service.service.ts
@@ -9,25 +9,50 @@ export class SocketioService {
 
   public socket: any;
 
+  public comments: { user: boolean, content: string }[] = [
+    { user: false, content: 'ご質問はございますか？' }
+  ];
+
   constructor() {}
 
+  // socket と 接続する
   public setupSocketConnection() {
 
     console.log('setupSocketConnection:', environment.SOCKET_ENDPOINT);
 
     this.socket = io(environment.SOCKET_ENDPOINT);
 
+    // ヘルプディスクからのメッセージを受け取る
     this.socket.on('HelpDesk->Customer', (data: string) => {
+      if(data == null)
+        return;
+      if(data.trim().length ===0)
+        return;
+
+      this.comments.push({ user: false, content: data });
+
       console.log("receive a message from HelpDesk:", data);
     });
 
   }
 
-  sendMessage(message: string) {
+  // ヘルプディスクにメッセージを送る
+  public sendMessage(message: string) {
+
+    if(message == null)
+      return;
+    if(message.trim().length ===0)
+      return;
+    
     this.socket.emit('Customer->HelpDesk', message);
+
+    this.comments.push({ user: true, content: message });
+
     console.log("send a message to HelpDesk: ", message);
   }
 
+
+  // socket から切断する
   public disconnect() {
     if (this.socket) {
       this.socket.disconnect();
