c8220671d5e1c63cade9d2bf99fc2f1889c4a6bc
sasaco <sasaicco@gmail.com>
2022-10-09 09:56:13 +0900

    グリッド入力スタイル

--- src/app/component/side-right-endbeam/side-right-endbeam.component.ts
+++ src/app/component/side-right-endbeam/side-right-endbeam.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import Handsontable from 'handsontable';
 import { GirderPalamService } from 'src/app/service/girder-palam.service';
 import { pvGirderService } from 'src/app/three/pvGirder.service';
 
@@ -18,4 +19,53 @@ export class SideRightEndbeamComponent {
       this.girder.createGirder(this.model.palam());
     }
 
+    private rowheader: string[] = [
+      'フランジ幅',
+      'フランジ厚',
+      'ウェブ厚',
+      '離隔（外側）',
+      '離隔（内側）'
+    ];
+
+    private dataset: any[] = [
+      {name: 'D5', value: this.model.endbeam.D5, unit: 'm'},
+      {name: 'tf4', value: this.model.endbeam.tf4, unit: 'm'},
+      {name: 'tw4', value: this.model.endbeam.tw4, unit: 'm'},
+      {name: 's_edge3', value: this.model.endbeam.s_edge3, unit: 'm'},
+      {name: 's_middle3', value: this.model.endbeam.s_middle3, unit: 'm'},    ];
+
+    private columns = [
+      {
+        data: 'unit',
+        readOnly: true
+      },
+      {
+        data: 'value',
+        type: 'numeric',
+        numericFormat: {
+          pattern: '0,0.000'
+        }
+      }
+    ];
+
+
+    public hotSettings: Handsontable.GridSettings = {
+      data: this.dataset,
+      colHeaders: false,
+      rowHeaders: this.rowheader,
+      columns: this.columns,
+      allowEmpty: false,
+      beforeChange: (changes, source)=>{
+        for(const [row, prop, oldValue, newValue] of changes){
+          let value = parseFloat(newValue);
+          if( isNaN(value) )
+            return false;
+          const name: string = this.dataset[row].name;
+          this.model.endbeam[name] = value;
+        }
+        // 再描画
+        this.redraw();
+        return true;
+      },
+    };
 }
--- src/app/component/side-right-others/side-right-others.component.ts
+++ src/app/component/side-right-others/side-right-others.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import Handsontable from 'handsontable';
 import { GirderPalamService } from 'src/app/service/girder-palam.service';
 import { pvGirderService } from 'src/app/three/pvGirder.service';
 
@@ -18,4 +19,59 @@ export class SideRightOthersComponent{
       this.girder.createGirder(this.model.palam());
     }
 
+    private rowheader: string[] = [
+      '起点端部から端横構までの離隔',
+      '終点端部から端横構までの離隔',
+      '端横構、荷重分配横桁、中間対傾構の総和',
+      '橋長',
+    ];
+
+    private dataset: any[] = [
+      {name: 's_BP', value: this.model.others.s_BP, unit: 'm'},
+      {name: 's_EP', value: this.model.others.s_EP, unit: 'm'},
+      {name: 'amount_H', value: this.model.others.amount_H, unit: '本'},
+      {name: 'L', value: this.model.others.L, unit: 'm'},
+    ];
+
+    private columns = [
+      {
+        data: 'unit',
+        readOnly: true
+      },
+      {
+        data: 'value',
+        type: 'numeric',
+        numericFormat: {
+          pattern: '0,0.000'
+        }
+      }
+    ];
+
+    private integer_cell: any[] = [
+      {row: 2, col: 2, type: 'numeric', numericFormat: {pattern: 'mantissa'}},
+    ];
+
+    public hotSettings: Handsontable.GridSettings = {
+      data: this.dataset,
+      colHeaders: false,
+      rowHeaders: this.rowheader,
+      columns: this.columns,
+      cell: this.integer_cell,
+      allowEmpty: false,
+      beforeChange: (changes, source)=>{
+        for(const [row, prop, oldValue, newValue] of changes){
+          let value = parseFloat(newValue);
+          if( isNaN(value) )
+            return false;
+          const name: string = this.dataset[row].name;
+          const isInteger = this.integer_cell.find( element => element.row === row);
+          if(isInteger != null)
+            value = Math.round(value);
+          this.model.others[name] = value;
+        }
+        // 再描画
+        this.redraw();
+        return true;
+      },
+    };
 }
